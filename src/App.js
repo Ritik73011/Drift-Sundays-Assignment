7ed41e1c53476e2658272686a2b5eb9af8332af5
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-04 13:34:24 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Home from './Pages/Home';
-
+import {Route,Routes} from 'react-router'
+import Service from './Pages/Service';
+import Navbar from './components/Navbar/Navbar'
 function App() {
   return (
     <div className="App">
-     <Home/>
+        <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/service' element={<Service/>}/>
+      </Routes>
     </div>
   );
 }

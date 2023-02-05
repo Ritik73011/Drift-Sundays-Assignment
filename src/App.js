baca2aad147adc229288ab8fd1f230828d2ffb8a
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-05 16:48:40 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import {Route,Routes} from 'react-router'
 import Service from './Pages/Service';
 import Navbar from './components/Navbar/Navbar'
 import Contact from './Pages/Contact';
+import PrivateRoute from './privateRouting/PrivateRoute'
+import AdminDashboard from './Pages/AdminDashboard'
+import AdminLogin from './Pages/AdminLogin';
+import {AdminContext} from './context/AdminContext'
+import { useState } from 'react';
 function App() {
+  const [login,setLogin] = useState(false);
+  const updateLogin = (val)=>{
+    setLogin(val);
+  }
   return (
     <div className="App">
         <Navbar/>
-      <Routes>
+        <AdminContext.Provider value={{login,updateLogin}}>
+        <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/service' element={<Service/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path='/admin-dashboard' element={<PrivateRoute children={<AdminDashboard/>}/>}/>
+        <Route path='/admin-login' element={<AdminLogin/>}/>
       </Routes>
+      </AdminContext.Provider>
     </div>
   );
 }

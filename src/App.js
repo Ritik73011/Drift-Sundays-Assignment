45a68c103abc4d2ed3b9c834d89777a3c980da76
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-06 20:28:14 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './Pages/Home';
-import {Route,Routes} from 'react-router'
+import {Route,Routes,useLocation} from 'react-router'
 import Service from './Pages/Service';
 import Navbar from './components/Navbar/Navbar'
 import Contact from './Pages/Contact';
@@ -9,11 +9,14 @@ import AdminDashboard from './Pages/AdminDashboard'
 import AdminLogin from './Pages/AdminLogin';
 import {AdminContext} from './context/AdminContext'
 import { useState } from 'react';
+import Footer from './components/Footer/Footer';
 function App() {
+  const location = useLocation();
   const [login,setLogin] = useState(false);
   const updateLogin = (val)=>{
     setLogin(val);
   }
+
   return (
     <div className="App">
         <Navbar/>
@@ -24,8 +27,9 @@ function App() {
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/admin-dashboard' element={<PrivateRoute children={<AdminDashboard/>}/>}/>
         <Route path='/admin-login' element={<AdminLogin/>}/>
-      </Routes>
+        </Routes>
       </AdminContext.Provider>
+      {location.pathname==='/admin-dashboard'?"":<Footer/>}
     </div>
   );
 }
